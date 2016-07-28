a242f7bd5f8c6739a7191df3adf3f4c35a5ea078
artemkaint <artemkaint@nodasoft.com>
2016-07-28 13:43:43 +0000

    Implement dynamic bootstrap submodules compilation

--- bootstrap-styles.loader.js
+++ bootstrap-styles.loader.js
@@ -11,12 +11,11 @@ function addImportReturnDependency(loader, config, propertyName) {
 
     logger.verbose(config, 'fileName for %s: %s', propertyName, fileNameResolved);
     loader.addDependency(fileNameResolved);
-    return '@import \'' + fileNameResolved + '\';\n';
+    return '@import \'' + fileNameResolved + '\';';
   }
 }
 
 module.exports = function(content) {
-  var source;
   var config = this.exec(content, this.resourcePath);
   var pathToBootstrap = bootstrapPath.getPath(this.context);
 
@@ -29,7 +28,10 @@ module.exports = function(content) {
     var unnecessary = Object.keys(config.styles).filter(function(style) {
       return !~partials.indexOf(style);
     });
-    logger.verbose(config, 'unnecessary bootstrap modules:', unnecessary.join(', '));
+
+    if (unnecessary.length) {
+      logger.verbose(config, 'unnecessary bootstrap modules:', unnecessary.join(', '));
+    }
   }
 
   var relativePathToBootstrap = path.relative(this.context, pathToBootstrap);
@@ -37,31 +39,34 @@ module.exports = function(content) {
   this.cacheable(true);
   logger.verbose(config, 'bootstrap location: %s', relativePathToBootstrap);
 
+  var makeImportString = function(relativePathToBootstrap, partial) {
+    return '@import \'' + path.join(relativePathToBootstrap, 'scss', partial) + '\';';
+  };
+
+  // Prepand minixs
+  var mixinPartial = 'mixins';
+  if (config.styles[mixinPartial] && !!~partials.indexOf(mixinPartial)) {
+    start += makeImportString(relativePathToBootstrap, mixinPartial) + '\n';
+  }
+
   if (config.preBootstrapCustomizations) {
-    start += addImportReturnDependency(this, config, 'preBootstrapCustomizations');
+    start += addImportReturnDependency(this, config, 'preBootstrapCustomizations') + '\n';
   }
-  start +=
-    // Absolute paths as these are created at build time.
-    '@import \'' + path.join(relativePathToBootstrap,
-      'scss/variables') + '\';\n';
-
+  start += makeImportString(relativePathToBootstrap, 'variables') + '\n';
   if (config.bootstrapCustomizations) {
-    start += addImportReturnDependency(this, config, 'bootstrapCustomizations');
+    start += addImportReturnDependency(this, config, 'bootstrapCustomizations') + '\n';
   }
 
-  source = start + partials.filter(function(partial) {
-      return config.styles[partial];
-    }).map(function(partial) {
-      return '@import \'' + path.join(relativePathToBootstrap, 'scss',
-          partial) + '\';';
-    }).join('\n');
+  var source = start + partials.filter(function(partial) {
+    return config.styles[partial] && !~['variables', mixinPartial].indexOf(partial);
+  }).map(function(partial) {
+    return makeImportString(relativePathToBootstrap, partial);
+  }).join('\n') + '\n';
 
   if (config.mainSass) {
-    source += '\n' + addImportReturnDependency(this, config, 'mainSass');
+    source += addImportReturnDependency(this, config, 'mainSass') + '\n';
   }
-
   source = source.replace(/\\/g, '/');
-
   logger.debug(config, 'Generated scss file is:\n' + source);
 
   return source;
